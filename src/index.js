91423abbc105424364c0aa8f18daf8c408eeed38
Achraf JS <75223132+achrafbouisk@users.noreply.github.com>
2022-12-27 01:22:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import App from "./App";
+
+
+const rootElement = document.getElementById("root");
+
+const root = createRoot(rootElement);
+
+root.render(
+
+  <React.StrictMode>
+
+    <App />
+
+  </React.StrictMode>
+
+);
+
